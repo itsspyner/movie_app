6d9339834fac03c22477e1ae9d2809478c3895f6
Mohit Joshi <160397729+itsspyner@users.noreply.github.com>
2025-06-25 17:30:27 +0545

    Delete test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-async function lol() {
-    const req = await fetch('http://www.omdbapi.com/?apikey=dc2fc370&s=Batman')
-    const res = await req.json();
-    res.Search.map((elem) => {
-        console.log(elem)
-    })
-}
-
-lol()
